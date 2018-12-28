81a084c2d2d250aaa0625a99cbc37150ecc8dcdc
liqiang <liqiang0335@qq.com>
2018-12-28 15:24:00 +0800

    add

--- ynw-md-factory.js
+++ ynw-md-factory.js
@@ -30,50 +30,54 @@ md.use(require("markdown-it-attrs"));
  */
 (function() {
   const filePath = process.argv[2];
+
   const dirname = path.dirname(filePath);
   const resolve = name => path.join(dirname, name);
+  const hasDistDir = fs.existsSync(resolve("dist"));
   const TARGET_NAME = "index.html";
 
-  //...
+  //读取 markdown
   const source = fs.readFileSync(filePath, "utf-8");
-  let html = md.render(source);
-  html = mapCustomTag(html);
-  fs.writeFileSync(resolve(TARGET_NAME), decorator(html));
+  let contents = md.render(source);
+  contents = addCustom(contents);
+  contents = addWrapper(contents, { hasDistDir });
+
+  fs.writeFileSync(resolve(TARGET_NAME), contents);
 })();
 
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
-function decorator(content) {
+function addWrapper(contents, { hasDistDir }) {
+  //是否包含dist文件夹
+  const scriptTag = hasDistDir
+    ? `<script src="https://cdn.bootcss.com/react/16.6.3/umd/react.production.min.js"></script>
+    <script src="https://cdn.bootcss.com/react-dom/16.6.3/umd/react-dom.production.min.js"></script>
+    <script src="./dist/index.bundle.js"></script>`
+    : "";
+
   return `
   <!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="maximum-scale=1.0,minimum-scale=1.0,user-scalable=0,width=device-width,initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/page/note/base/dist/base.bundle.css"/>
+    <link rel="stylesheet" href="https://www.jsgaotie.com/page/note/base/dist/base.bundle.css"/>
   </head>
   <body>
-  <div id="app">${content}</div>
-  <script src="https://cdn.bootcss.com/react/16.6.3/umd/react.production.min.js"></script>
-  <script src="https://cdn.bootcss.com/react-dom/16.6.3/umd/react-dom.production.min.js"></script>
-  <script src="./dist/index.bundle.js"></script>
+  <div id="app">${contents}</div>
+  ${scriptTag}
   </body>
   </html>
   `;
 }
 
-function mapCustomTag(source) {
-  return source.replace(/@{(.+?)}/g, (match, value) => {
-    const arr = value.split(" ");
-    const attrs = arr
-      .map(item => {
-        const [key, value] = item.split("=");
-        return `${key}="${value}"`;
-      })
-      .join(" ");
-    return `<div ${attrs}></div>`;
-  });
+function addCustom(contents) {
+  contents = contents.replace(
+    /<li>(.+?)[:：](.+<\/li>)/g,
+    `<li><span class="li-text">$1</span><span class="li-colon">:</span>$2</li>`
+  );
+  return contents;
 }
